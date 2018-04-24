fc53cdca955d8d46957eb94c3e25d6fa65dffebd
Stevie <Steven.Ulfelder@vermont.gov>
2018-04-24 14:21:33 -0400

    New experience button writes dummy data to MySQL!!!!

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,10 +1,19 @@
+
+
 $('#addLineButton').click(function() {
   //alert( $('#textInputBox').val() );
+  if($('#textInputBox').val() == ""){
+    return;
+  }
   var $div = $("<div>", {"class": "reportLine", "text": $('#textInputBox').val()});
   $('#displayContainer').append($div);
+  $.getJSON($SCRIPT_ROOT + '/_test', {linetext: $('#textInputBox').val()}, function(data){ console.log(data.result); });
   document.getElementById('textInputBox').value = "";
 });
 
+$('#newExperienceButton').click(function() {
+  $.getJSON($SCRIPT_ROOT + '/_new_report', {substance: 'LSD', dosage: 20, dosagelabel: 'ug', source: 'local'}, function(data){ console.log(data.result); });
+});
 
 $("#textInputBox").keydown(function(e){
 if (e.keyCode == 13 && !e.shiftKey)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 # Flask views
-from flask import render_template
+from flask import render_template, jsonify, request
+import time
+import datetime
 
 from app import app, db
 from app import models
@@ -16,3 +18,26 @@ def about():
 @app.route('/database')
 def database():
     return render_template("mockup.html")#Report.query.all()[0].Substance
+
+@app.route('/_test')
+def test():
+    linetext = request.args.get('linetext', "null", type=str)
+    linetext = linetext + " boo!"
+    return jsonify(result=linetext)
+
+@app.route('/_new_report')
+def new_report():
+    rawTime = time.time()
+    startDate = datetime.datetime.fromtimestamp(rawTime).strftime('%Y-%m-%d %H:%M:%S')
+    substance = request.args.get('substance', "null", type=str)
+    dosage = request.args.get('dosage', 0, type=int)
+    dosagelabel = request.args.get('dosagelabel', "null", type=str)
+    source = request.args.get('source', "null", type=str)
+
+    tester = startDate + substance + str(dosage) + dosagelabel + source
+
+    newReport = Report(StartDate=startDate, Substance=substance, Dosage=dosage, DosageLabel=dosagelabel, Source=source)
+    db.session.add(newReport)
+    db.session.commit()
+    
+    return jsonify(result=tester)
